51d5112865ce94ecf9adc0ad9144c3e700e6599c
SaM27my <saamanmy@gmail.com>
2022-03-10 16:09:12 +0300

    the first c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+*main print string from main function
+*
+*Description using the puts function this program 
+*prints "programming is like building multilingual puzzle
+* return 0
+*/
+int main(void)
+{
+	puts("\"programming is like building multilingual puzzle");
+
+return (0);
+}
