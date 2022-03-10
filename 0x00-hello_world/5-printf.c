ea3a8ce4a582bea843ac7acd0b1e7f7b5b53f055
SaM27my <saamanmy@gmail.com>
2022-03-10 16:36:04 +0300

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 /**
 * main - print strings using printf function
 *
-* Description: using the main function 
+* Description: using the main function
 * the program prints with proper grammar, but the outcome is a piece of art,\n
 * Return: 0
 */
@@ -10,6 +10,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 }
