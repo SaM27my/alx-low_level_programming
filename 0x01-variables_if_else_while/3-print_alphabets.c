808ed3bc56b2a89a089b92870e543e1d3d7a2b9f
SaM27my <saamanmy@gmail.com>
2022-03-11 13:28:25 +0300

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+*main - uses while loop
+*
+*Description: printing aplhabet in lower case and upper case  followed by new line
+*Return: 0
+*/
+
+int main (void)
+{
+
+        char ch1 = 'a';
+	char ch2 = 'A';
+
+        while (ch1 <= 'z')
+	{
+		putchar(ch1);
+                ch1++;
+        }
+	while (ch2 <= 'Z')
+	{
+	putchar(ch2);
+	ch2++;
+	}
+	putchar('\n');
+
+	return (0);
+}
