b5a9f376c3d5856ed7b4210ca8d712362eed65d7
SaM27my <saamanmy@gmail.com>
2022-03-11 16:52:15 +0300

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+*main - no functionsother than putcha
+*
+* Description: this program all the hexadecimal values in lower case
+*Return: 0
+*/
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+	putchar(c);
+	c++;
+	}
+
+	c = 'a';
+
+	while (c <= 'f')
+	{
+	putchar(c);
+	c++;
+	}
+	putchar('\n');
+}
