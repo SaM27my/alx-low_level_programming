a25dbbb52a66832cb9dad1b1ee1d2074b1bf2cc9
SaM27my <saamanmy@gmail.com>
2022-03-11 15:54:20 +0300

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main -the main function uses while loop function
+*
+* Description: print base ten single digit numbers
+*
+* Return:0
+*/
+int main(void)
+{
+
+	char n = '0';
+
+	while (n <= '9')
+	{
+	putchar(n);
+	n++;
+	}
+	putchar('\n');
+	return (0);
+}
