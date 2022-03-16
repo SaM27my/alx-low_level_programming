8ed146b8a451fdafa2b07e79619ad15bbf2318b3
SaM27my <saamanmy@gmail.com>
2022-03-16 20:16:14 +0300

    together

---
+++ 0x02-functions_nested_loops/forfun.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(void){
+
+for (int i=0; i<3; i++){
+	for (int j=0; j<6; j++){
+		for (int k=0;k<6;k++){
+			for (int l=0; l<10; l++){
+				printf("%i%i:%i%i\n", i, j ,k ,l);
+				}
+
+			}
+
+
+		}
+
+
+	}
+return (0);
+}
