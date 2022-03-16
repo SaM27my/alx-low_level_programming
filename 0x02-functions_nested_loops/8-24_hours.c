a4b69bd1b54a36dedf51f2f6658997853f2547f7
SaM27my <saamanmy@gmail.com>
2022-03-16 17:26:01 +0300

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,19 +12,22 @@ void jack_bauer(void)
 
 	for (i = 0; i < 3; i++)
 	{
-		for (j = 0; j < 4; j++)
+		for (j = 0; j < 10; j++)
 		{
 			for (k = 0; k < 6; k++)
 			{
 				for (l = 0; l < 10; l++)
+				{
+					if (k != 2 || l < 3)
 					{
-					_putchar(i + '0');
-					_putchar(j + '0');
-					_putchar(':');
-					_putchar(k + '0');
-					_putchar(l + '0');
-					_putchar('\n');
+						_putchar(i + '0');
+						_putchar(j + '0');
+						_putchar(':');
+						_putchar(k + '0');
+						_putchar(l + '0');
+						_putchar('\n');
 					}
+				}
 			}
 		}
 	}
