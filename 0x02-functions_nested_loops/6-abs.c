7532b759b064907b6508d98dc9ff3030d349974d
SaM27my <saamanmy@gmail.com>
2022-03-16 13:11:23 +0300

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _abs - prints the signs of an integer
+* @n: is the integer whose value is evaluated
+* Descriptio: return n for positive n 0 for zero
+* and -n for negative n
+* Return: -n for -ve 0 for zero and n for +ve
+*/
+int _abs(int n)
+{
+	if (n >= 0)
+	return (n);
+
+	else
+	return (-n);
+}
