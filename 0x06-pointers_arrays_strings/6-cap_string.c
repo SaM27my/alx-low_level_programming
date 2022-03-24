c16a9da2ad44b395e1c79bc084e5d51987876cce
SaM27my <saamanmy@gmail.com>
2022-03-24 12:44:22 +0300

    Create 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * cap_string - capitalize after separator words
+ * @s: character to capitalize after separator
+ * Return: return value of upper
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		while(s[i] == ' ' || s[i] == '\t' || s[i] == '\n' || s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?' || s[i] == '"' || s[i] == '(' || s[i] == ')' ||s[i] == '{' || s[i] == '}' )
+		{
+			if (s[i+1] >= 'a' && s[i+1] <= 'z')
+			{
+				s[i+1] -= 32;
+			}
+		}	
+	}
+	return (s);
+}
