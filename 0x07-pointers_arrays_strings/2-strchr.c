984cd0482edde02b60c3600224c36b37721d141b
SaM27my <saamanmy@gmail.com>
2022-03-28 17:31:54 +0300

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,11 +8,11 @@
 */
 char *_strchr(char *s, char c)
 {
-	do
-	{
-	if (*s == c)
-		return(s);
-	} while (*s++)
+	do {
 
-	return(0);
+	if (*s == c)
+		return (s);
+	} while (*s++);
+
+	return (0);
 }
