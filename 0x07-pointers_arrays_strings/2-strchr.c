a6045f4602495db9cad99efb3318cc706539f1ca
SaM27my <saamanmy@gmail.com>
2022-03-28 17:26:26 +0300

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _strchr - searches for a character in a string
+*
+* @s: string
+* @c: character to search
+* Return: 0
+*/
+char *_strchr(char *s, char c)
+{
+	do
+	{
+	if (*s == c)
+		return(s);
+	} while (*s++)
+
+	return(0);
+}
