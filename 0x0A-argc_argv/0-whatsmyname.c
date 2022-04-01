2231a2c7c58a7d43ed7f90f50aa93d3e6285fe76
SaM27my <saamanmy@gmail.com>
2022-04-01 22:48:46 +0300

    0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - prints its name
+*
+* Description: this program prints its name
+*
+* Return: always 0
+*/
+int main(int argc, char** argv)
+{
+	printf(argv[0], "\n");
+	printf("\n");
+	return (0);
+}
